8958ddf313a8dd671c384b1db479856ca1aed23e
Thang Nguyen <thangnguyen0722@gmail.com>
2023-09-06 11:17:03 +0700

    Updated for Section 2

---
+++ BaoLam/BaoLamWeb/Controllers/CategoryController.cs
@@ -0,0 +1,106 @@
+﻿using BaoLamWeb.Data;
+using BaoLamWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaoLamWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _dbContext.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Add(obj);
+                _dbContext.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _dbContext.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Update(obj);
+                _dbContext.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _dbContext.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _dbContext.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Remove(obj);
+                _dbContext.SaveChanges();
+                TempData["success"] = "Category deleted successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ BaoLam/BaoLamWeb/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BaoLamWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order mus be between 1 - 100")]
+        public int DisplayOrder { get; set; }
+    }
+}
